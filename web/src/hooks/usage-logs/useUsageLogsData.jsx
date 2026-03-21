29e3e4b165c5a2add92ffaf978b2798a91f3acf7
handsomeqin <qinguanjun@gmail.com>
2026-03-22 01:27:31 +0800

    隐藏模型2

--- web/src/hooks/usage-logs/useUsageLogsData.jsx
+++ web/src/hooks/usage-logs/useUsageLogsData.jsx
@@ -467,10 +467,6 @@ export const useLogsData = () => {
             key: t('请求并计费模型'),
             value: logs[i].model_name,
           });
-          expandDataLocal.push({
-            key: t('实际模型'),
-            value: other.upstream_model_name,
-          });
         }
 
         const isViolationFeeLog =
